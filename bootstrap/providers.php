ea225f19207cb840cc361c4e856e85e8b8b5caf7
ineptunus <rayarskt@gmail.com>
2026-06-29 08:18:23 +0700

    fix bootstrap providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,21 +1,5 @@
 <?php
 
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-
-class AppServiceProvider extends ServiceProvider
-{
-    public function register()
-    {
-        //
-    }
-
-    public function boot()
-    {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
-    }
-}
+return [
+    App\Providers\AppServiceProvider::class,
+];
