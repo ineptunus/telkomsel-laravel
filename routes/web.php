060d376d82ae0232c4963562d527cf952e631671
ineptunus <rayarskt@gmail.com>
2026-06-29 08:27:36 +0700

    add register route back

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,21 @@ use App\Http\Controllers\ExportController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PredictionController;
 
+
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.process');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.process');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+
 Route::post('/api/predict-tweet', [PredictionController::class, 'predict']);
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
